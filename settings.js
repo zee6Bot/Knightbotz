6d9d125b67d48cdd64db06c9e7d7908ac2442b5a
zee6Bot <zee996339@mailpro.rest>
2025-01-30 09:52:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Negan Bot",
+  botOwner: 'Gaushik', // Your name
+  ownerNumber: '917306060208', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
